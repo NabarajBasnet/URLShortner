193d4f4e44375d860cefbf36b9e845d35aacaa57
Nabaraj Basnet <Nabaraj Basnet>
2025-04-05 10:55:04 +0545

    EJS templates creating

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+const { nanoid } = require('nanoid');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { shortUrl: null })
+});
+
+app.post('/shorten', async (req, res) => {
+    const { longUrl } = req.body;
+    const shortId = nanoid(6);
+    const shortUrl = `${req.protocol}://${req.get('host')}/${shortId}`;
 
 
+    res.render('success', { shortUrl: shortUrl });
+    console.log("Short URL: ", shortUrl);
 
+});
 
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`);
 });
